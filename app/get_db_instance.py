2be823c9025e20b71b576552edfc294f8ff1d2a0
Tinker-Bits <jashupadhyay.java@gmail.com>
2025-07-27 13:56:29 +0530

    Now Using Open Source Model Instead Of Paid Model

--- app/get_db_instance.py
+++ app/get_db_instance.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from langchain_community.utilities.sql_database import SQLDatabase
 load_dotenv()
-openai_api_key = os.getenv('OPEN_AI_KEY')
 database_user = os.getenv('DB_USER') 
 database_password = os.getenv('DB_PASSWORD') 
 database_host = os.getenv('DB_HOST')  
